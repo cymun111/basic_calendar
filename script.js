49dca772a7c4251ccf554dbb3e789953b099d8ed
cymun111 <cymun111@gmail.com>
2020-08-12 12:50:45 -0700

    first commit

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
+$(document).ready(function(){
 const date = new Date();
 
 const renderCalendar = () => {
   date.setDate(1);
 
-  const monthDays = document.querySelector(".days");
 
-  const lastDay = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDate();
+
+  const monthDays = [];
+
+  // First Day of the Calendar
+  const firstDay = date.getDay();
+  console.log("First" + firstDay)
 
   const prevLastDay = new Date(
     date.getFullYear(),
     date.getMonth(),
     0
   ).getDate();
+  console.log(prevLastDay)
 
-  const firstDayIndex = date.getDay();
+  for (let x = firstDay; x > 0; x--) {
+   let  days = prevLastDay - x + 1;
+   monthDays.push(days)
+  }
+  
+
+  // Last day of the month
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  for (let i = 1; i <= lastDay; i++) {
+    monthDays.push(i);
+  }
 
   const lastDayIndex = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
   ).getDay();
+  console.log("Last" + lastDay)
 
+// Last Day of the Calendar
   const nextDays = 7 - lastDayIndex - 1;
+  console.log("Next" + nextDays)
 
+
+  for (let j = 1; j <= nextDays; j++) {
+    monthDays.push(j)
+  }
+  console.log(monthDays.length)
+  let length = monthDays.length
+
+  for(let i = 1; i <= length ; ++i){
+    let day = monthDays.shift()
+    $('.tableBody').append(`<td class="col-md-2">${day}</td>`)
+    if(i % 7 == 0){
+      console.log('true')
+      $('.tableBody').append(`<tr></tr>`)
+    }
+
+  }
   const months = [
     "January",
     "February",
@@ -42,48 +78,24 @@ const renderCalendar = () => {
     "December",
   ];
 
-  document.querySelector(".date h1").innerHTML = `${months[date.getMonth()]}  ${date.getFullYear()}`;
+  document.querySelector(".date h1").innerHTML = `${months[date.getMonth()]} ${date.getFullYear()}` ;
 
   document.querySelector(".date p").innerHTML = new Date().toDateString();
 
-  let days = "";
 
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<td class="prev-date col-md-2">${prevLastDay - x + 1}</td>`;
-  }
-
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<td class="today col-md-2">${i}</td>`;
-    } else {
-      days += `<td class="col-md-2">${i}</td>`;
-    }
-  }
-
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<td class="next-date col-md-2">${j}</td>`;
-    // monthDays.innerHTML = days;
-  }
-  for(let x = 1; x <= totalDays; x++){
-    for(let x = 1; x <= 7; x++){
-      `<tr></tr>`
-      monthDays.innerHTML = days;
-    }
-  }
-  console.log(days)
 };
 
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
+  $('.tableBody').empty()
   renderCalendar();
 });
 
 document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
+  $('.tableBody').empty()
   renderCalendar();
 });
 
 renderCalendar();
+}) 
